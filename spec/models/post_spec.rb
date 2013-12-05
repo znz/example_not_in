40eaef129c78f591a4f79b09e42782df0fd0e2c4
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-12-05 11:12:38 +0900

    votes_voter_id_in and votes_voter_id_not_in

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,32 @@ describe Post do
   it do
     expect(FactoryGirl.create(:post)).to be_a Post
   end
+
+  context "with 2 posts" do
+    before do
+      2.times do
+        FactoryGirl.create(:post)
+      end
+    end
+    let(:user) { FactoryGirl.create(:user) }
+
+    it { expect(Post.count).to eq(2) }
+
+    it "no voted posts" do
+     expect(Post.search(votes_voter_id_in: user.id).result(distinct: true).count).to eq(0)
+    end
+    it "two unvoted posts" do
+     expect(Post.search(votes_voter_id_not_in: user.id).result(distinct: true).count).to eq(2)
+    end
+    context "with one voted post" do
+      before { user.vote_for(Post.first) }
+
+      it "one voted post" do
+        expect(Post.search(votes_voter_id_in: user.id).result(distinct: true).count).to eq(1)
+      end
+      it "one unvoted post" do
+        expect(Post.search(votes_voter_id_not_in: user.id).result(distinct: true).count).to eq(1)
+      end
+    end
+  end
 end
