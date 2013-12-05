45036108064b95dbd8a06ce508214b542d386202
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-12-05 10:49:52 +0900

    `rails new example_not_in -B -T`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExampleNotIn::Application.config.secret_key_base = '6aa2254978a2625ff12413c39aa746a554089636f792d58fe0cfda8dc849308027b413442900477c7a1d593027cd1eb61afc001843113e95ab431403af0da4e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExampleNotIn::Application.config.session_store :cookie_store, key: '_example_not_in_session'
